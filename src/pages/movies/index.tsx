9cd2ee5c732ccb497072ab662036b6c344251653
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-08-22 23:16:15 +0700

    Solve Imam Renaldy

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -20,20 +20,24 @@ class Movies extends Component<MoviesState> {
   };
 
   getNowPlaying(page: number) {
-    console.log("page : ", page);
+
+    const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWQ4NTE0ZmViMzI4YzZiNzhhZWY1YzEyNjE3NDIwOSIsInN1YiI6IjYxYTgzYzhhODc1ZDFhMDA4YjU4OWJhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.19IVAC2nzdtIHIPkcEN5sK0QBGIRwKyUmtUF_2QJiO0"
+    console.log("page Now Playing : ", page);
     axios
       .get(
         `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`,
         {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWQ4NTE0ZmViMzI4YzZiNzhhZWY1YzEyNjE3NDIwOSIsInN1YiI6IjYxYTgzYzhhODc1ZDFhMDA4YjU4OWJhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.19IVAC2nzdtIHIPkcEN5sK0QBGIRwKyUmtUF_2QJiO0`,
+            Authorization: `Bearer ${API_KEY}`,
           },
         }
       )
       .then((response) => {
-        this.setState({ loading: true });
-        this.setState({ data: response?.data?.results });
-        this.setState({ page: response?.data?.page });
+        this.setState({
+          loading: true,
+          data: response?.data?.results,
+          page: response?.data?.page
+        });
         console.log(response?.data?.page);
       })
       .catch((error) => {
@@ -41,36 +45,124 @@ class Movies extends Component<MoviesState> {
       });
   }
 
+  // async getPrevious() {
+  //   const { page } = this.state;
+  //   this.setState({ page: page - 1 });
+  //   const pagination = page;
+  //   this.setState({ index: pagination });
+  // }
+
   async getPrevious() {
     const { page } = this.state;
-    this.setState({ page: page - 1 });
-    const pagination = page;
-    this.setState({ index: pagination });
+    const pagination = page - 1; // Menghitung halaman berikutnya
+    this.setState({ page: pagination, index: pagination }); // Mengubah state page dan index
+    await this.getNowPlaying(pagination); // Memanggil getNowPlaying dengan halaman berikutnya
   }
+
+  // async getNext() {
+  //   const { page } = this.state;
+  //   this.setState({ page: page + 1 });
+  //   const pagination = page;
+  //   this.setState({ index: pagination });
+  //   this.getNowPlaying(pagination);
+  // }
 
   async getNext() {
     const { page } = this.state;
-    this.setState({ page: page + 1 });
-    const pagination = page;
-    this.setState({ index: pagination });
+    const pagination = page + 1; // Menghitung halaman berikutnya
+    this.setState({ page: pagination, index: pagination }); // Mengubah state page dan index
+    await this.getNowPlaying(pagination); // Memanggil getNowPlaying dengan halaman berikutnya
   }
+
 
   componentDidMount(): void {
     const { index } = this.state;
+    console.log("index compount: ", index);
     this.getNowPlaying(index);
   }
 
+  // render() {
+  //   const { data, loading, page, index } = this.state;
+  //   console.log("index render : ", index);
+  //   console.log("page : ", page);
+
+  //   const previousButtonClass = page <= 1
+  //   ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+  //   : "bg-alta-blue-dark text-white";
+
+  //   return (
+  //     <section className="flex flex-col justify-center">
+  //       <div className="flex flex-row mx-auto my-10">
+  //         <button
+  //           className={`${page <= 1
+  //               ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+  //               : "bg-alta-blue-dark text-white"
+  //             } focus:outline-none w-40 mr-5`}
+  //           onClick={() => this.getPrevious()}
+  //         >
+  //           Previous
+  //         </button>
+  //         <button
+  //           className="bg-alta-blue-dark text-white focus:outline-none w-40 ml-5"
+  //           onClick={() => this.getNext()}
+  //         >
+  //           Next
+  //         </button>
+  //       </div>
+  //       <div className="m-5 grid grid-cols-5 gap-x-5 gap-y-10">
+  //         {data && loading === true ? (
+  //           data.map((item: any, index) => {
+  //             return (
+  //               <MoviesCard
+  //                 key={index}
+  //                 id={item?.id.toString()}
+  //                 title={item?.title}
+  //                 poster_path={item?.poster_path}
+  //                 vote_average={item?.vote_average}
+  //               />
+  //             );
+  //           })
+  //         ) : (
+  //           <Modal isOpen={true}>
+  //             <div className="w-40 text-center my-20">
+  //               <p className="font-semibold">Please wait ...</p>
+  //             </div>
+  //           </Modal>
+  //         )}
+  //       </div>
+  //     </section>
+  //   );
+  // }
+
   render() {
     const { data, loading, page, index } = this.state;
+    console.log("index render : ", index);
+    console.log("page : ", page);
+    const previousButtonClass = page <= 1
+      ? "bg-gray-400 text-gray-700 cursor-not-allowed"
+      : "bg-alta-blue-dark text-white";
+    const movieCards = data && loading === true
+      ? data.map((item: any, index) => (
+        <MoviesCard
+          key={index}
+          id={item?.id.toString()}
+          title={item?.title}
+          poster_path={item?.poster_path}
+          vote_average={item?.vote_average}
+        />
+      ))
+      : (
+        <Modal isOpen={true}>
+          <div className="w-40 text-center my-20">
+            <p className="font-semibold">Please wait ...</p>
+          </div>
+        </Modal>
+      );
     return (
       <section className="flex flex-col justify-center">
         <div className="flex flex-row mx-auto my-10">
           <button
-            className={`${
-              page <= 1
-                ? "bg-gray-400 text-gray-700 cursor-not-allowed"
-                : "bg-alta-blue-dark text-white"
-            } focus:outline-none w-40 mr-5`}
+            className={`${previousButtonClass} focus:outline-none w-40 mr-5`}
             onClick={() => this.getPrevious()}
           >
             Previous
@@ -83,25 +175,7 @@ class Movies extends Component<MoviesState> {
           </button>
         </div>
         <div className="m-5 grid grid-cols-5 gap-x-5 gap-y-10">
-          {data && loading === true ? (
-            data.map((item: any, index) => {
-              return (
-                <MoviesCard
-                  key={index}
-                  id={item?.id.toString()}
-                  title={item?.title}
-                  poster_path={item?.poster_path}
-                  vote_average={item?.vote_average}
-                />
-              );
-            })
-          ) : (
-            <Modal isOpen={true}>
-              <div className="w-40 text-center my-20">
-                <p className="font-semibold">Please wait ...</p>
-              </div>
-            </Modal>
-          )}
+          {movieCards}
         </div>
       </section>
     );
